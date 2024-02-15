9d187f07eae7cec98d52be79bd350cd8601abdfe
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-16 00:37:03 +0530

    tower of hanoi

---
+++ Practice/test14.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class test14 {
+    public static void main(String[] args) {
+        // tower of hanoi
+        int n = 3;
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }
+
+    private static void towerOfHanoi(int n, char from, char to, char aux) {
+        if (n == 0) {
+            return;
+        }
+        towerOfHanoi(n - 1, from, aux, to);
+        System.out.println("Moving disk " + n + " from : " + from + " to " + to);
+        towerOfHanoi(n - 1, aux, to, from);
+    }
+}
