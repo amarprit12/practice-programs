26c453a4f9377f4362555ecc1167ce5472055e04
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-16 23:29:02 +0530

    left view of binary tree

---
+++ Practice/BinaryTree1.java
@@ -0,0 +1,47 @@
+package Practice;
+
+public class BinaryTree1 {
+    Node root;
+    static int maxLevel = 0;
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree1 tree1 = new BinaryTree1();
+        tree1.root = new Node(1);
+        tree1.root.left = new Node(2);
+
+        tree1.root.right = new Node(3);
+        tree1.printLeftView(tree1.root);
+    }
+
+    private void printLeftView(Node root) {
+        if (root == null) {
+            return;
+        }
+        printLeftViewUtil(root, 1);
+
+    }
+
+    private void printLeftViewUtil(Node root, int level) {
+        if (root == null) {
+            return;
+        }
+        if (maxLevel < level) {
+            System.out.print(root.data + " ");
+            maxLevel = level;
+
+        }
+        printLeftViewUtil(root.left, level + 1);
+        printLeftViewUtil(root.right, level + 1);
+
+    }
+}
