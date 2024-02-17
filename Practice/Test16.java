c9c705d46ad739ff52dd25e2b24af7f630e9341e
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-17 22:55:01 +0530

    min and max element of array

---
+++ Practice/Test16.java
@@ -0,0 +1,31 @@
+package Practice;
+
+public class Test16 {
+    public static void main(String[] args) {
+//        int[] a = {1, 423, 6, 46, 34, 23, 13, 53, 4};
+        int[] a = {12, 1234, 45, 67, 1};
+        findMinimumElement(a, a.length);
+        findMaximumElement(a, a.length);
+    }
+
+    private static void findMinimumElement(int[] a, int n) {
+        int minimum = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (a[i] < minimum) {
+                minimum = a[i];
+            }
+        }
+        System.out.println("Minimum value is : " + minimum);
+    }
+
+
+    private static void findMaximumElement(int[] a, int n) {
+        int maximum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (a[i] > maximum) {
+                maximum = a[i];
+            }
+        }
+        System.out.println("Maximum value is : " + maximum);
+    }
+}
