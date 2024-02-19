b3440d4ef5f304de0d5e55b4587967fbad7f7858
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-19 17:45:04 +0530

    find triplets

---
+++ Practice/test17.java
@@ -0,0 +1,22 @@
+package Practice;
+
+public class test17 {
+    public static void main(String[] args) {
+        int[] a = {0, -1, 2, -3, 1};
+        int n = a.length;
+        findTriplets(a, n);
+    }
+
+    private static void findTriplets(int[] a, int n) {
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i + 1; j < n-1; j++) {
+                for (int k = i + 1; k < n; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        System.out.println(" " + a[i] + "  " + a[j] + "  " + a[k]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
