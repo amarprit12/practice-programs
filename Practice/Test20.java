e4714fb46c306554f3c576c4015a856400d46c2a
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-20 22:49:48 +0530

    number occurring odd no of times

---
+++ Practice/Test20.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Test20 {
+    public static void main(String[] args) {
+//        int[] a = {1, 2, 3, 2, 3, 1, 3};
+        int[] a = {5, 7, 2, 7, 5, 2, 5};
+        int n = a.length;
+        findNumOccurringOdd(a, n);
+    }
+
+    private static void findNumOccurringOdd(int[] a, int n) {
+        int x1 = 0;
+        for (int i = 0; i < n; i++) {
+            x1 ^= a[i];
+        }
+        System.out.println(x1);
+    }
+
+}
